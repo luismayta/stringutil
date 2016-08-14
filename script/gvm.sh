6f943d6b75d029936e8069e9d068bb83f7101b32
@slovacus <slovacus@gmail.com>
2016-08-14 16:15:10 -0500

    [#4] Fixed make paths on gopath

--- script/gvm.sh
+++ script/gvm.sh
@@ -9,9 +9,11 @@ gvm pkgset list | grep -q "${GVM_NAME}"
 response=$?
 
 if [[ ! "${response}" -eq '0' ]]; then
-    "$(gvm pkgset create "${GVM_NAME}")"
+    gvm pkgset create "${GVM_NAME}"
 fi
 
 gvm pkgset use "${GVM_NAME}"
 
-mkdir -p "${GOPATH}/${GVM_PATHS_NAME}"
+mkdir -p "${GOPATH}/src"
+mkdir -p "${GOPATH}/pkg"
+mkdir -p "${GOPATH}/bin"
